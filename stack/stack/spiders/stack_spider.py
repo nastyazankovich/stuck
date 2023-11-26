70164c3662bfe10cb40e5f6146a434096a31a5f8
Nastya <zankovichnastya@gmail.com>
2023-11-26 19:10:05 +0300

    Parse questions

---
+++ stack/stack/spiders/stack_spider.py
@@ -0,0 +1,17 @@
+import scrapy
+
+
+class StackOverflowSpider(scrapy.Spider):
+    name = 'stack'
+    start_urls = ['https://ru.stackoverflow.com/questions?tab=Newest']
+
+    def parse(self, response):
+        questions = response.css('.question-summary')
+        for question in questions:
+            title = question.css('.question-hyperlink::text').get()
+            description = question.css('.excerpt::text').get()
+            yield {
+                'title': title,
+                'description': description
+            }
+
